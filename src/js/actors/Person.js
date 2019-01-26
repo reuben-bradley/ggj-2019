1fd1de43dba9cef2da1660fed5fab49d048b12c9
Neil McCallion <nmccallion@shopkeep.com>
2019-01-26 16:06:48 +0000

    Add Babel for class transforms; person spawning; happiness tick

--- src/js/actors/Person.js
+++ src/js/actors/Person.js
@@ -3,21 +3,54 @@ import config from '../config/config';
 class Person extends Phaser.GameObjects.Sprite {
   constructor(scene, standingPos, partyPrefs) {
     super(scene, standingPos.x, standingPos.y, 'person');
+
     this.standingPos = standingPos;
+    this.depth = standingPos.y;
     this.partyPrefs = partyPrefs;
-    this.happiness = 50;
+    this.prevHappiness = this.happiness = 50;
+
+    // Add meter
+    this.setOrigin(0.5, 1)
+    this.meterText = new Phaser.GameObjects.Text(
+      scene, standingPos.x, standingPos.y - this.height, '', config.textStyles.meter
+    );
+    this.meterText.setOrigin(0.5, 1);
+    this.meterText.depth = standingPos.y + 100;
+    this.displayHappiness();
+    scene.add.existing(this.meterText);
   }
 
   update() {
     // animate etc
   }
 
-  doPartyTic(partyState) {
+  doPartyTic = (partyState) => {
     // compare partyState to prefs, adjust happiness
-    // act accordingly
+    let happinessDiff = 0;
+
+    for (const pref in this.partyPrefs) {
+      const prefVal = this.partyPrefs[pref];
+      const partyVal = partyState[pref];
+      // Reduce happiness for each unmet pref
+      if (partyVal !== prefVal) {
+        happinessDiff += config.happinessDecline;
+      }
+    }
+    // Only grow happiness if all prefs are met
+    this.happiness += (happinessDiff < 0) ? happinessDiff : config.happinessGrowth;
+    this.happiness = Phaser.Math.Clamp(this.happiness, 0, 100);
+    this.displayHappiness();
+    this.prevHappiness = this.happiness;
   }
 
-  joinParty() {
+  displayHappiness = () => {
+    let adjTxt = '';
+    if (this.prevHappiness < this.happiness) adjTxt = '+';
+    if (this.prevHappiness > this.happiness) adjTxt = '-';
+    this.meterText.setText(`${this.happiness} ${adjTxt}`);
+  }
+
+  enterParty() {
     // spawn at party entrance and move to standingPos
   }
 
--- src/js/scenes/play.js
+++ src/js/scenes/play.js
@@ -13,9 +13,56 @@ export default class Play extends Phaser.Scene {
   }
 
   create() {
-    console.log('Play create');
+    console.log('Play create', this);
 
-    const newPerson = new Person(this, { x:200, y:400 }, {});
+    this.partyState = {
+      heat: 0,
+      light: 0,
+      volume: 0
+    };
+    this.partyPrefNames = Object.keys(this.partyState);
+
+    this.partyPeople = [];
+    this.addNewRandomPerson();
+
+    this.partyTickTimer = this.time.addEvent({
+      delay: config.partyTickTime,
+      callback: this.doPartyTic,
+      loop: true
+    });
+
+    this.personSpawnTimer = this.time.addEvent({
+      delay: config.newPersonSpawnTime,
+      callback: this.addNewRandomPerson,
+      loop: true
+    })
+  }
+
+  addNewRandomPerson = () => {
+    if (this.partyPeople.length === config.maxPersons) return false;
+
+    const specificPref = Phaser.Utils.Array.GetRandom(this.partyPrefNames);
+    const prefValue = Phaser.Math.RND.integerInRange(0, 1);
+
+    this.addPartyPerson({
+        x: Phaser.Math.RND.integerInRange(100, 700),
+        y: Phaser.Math.RND.integerInRange(300, 500)
+      },
+      {
+        [specificPref]: prefValue
+      }
+    );
+  }
+
+  addPartyPerson = (startingPos, prefs) => {
+    const newPerson = new Person(this, startingPos, prefs);
     this.add.existing(newPerson);
+    this.partyPeople.push(newPerson);
+  }
+
+  doPartyTic = () => {
+    this.partyPeople.forEach(
+      (person) => person.doPartyTic(this.partyState)
+    );
   }
 }
